d2a4670b073d5e767167ccc1981a1a3eaabc04e2
PinkiesRevenge <rabidchipmonk@gmail.com>
2022-09-30 09:13:48 -0400

    player movment

--- script.js
+++ script.js
@@ -6,7 +6,22 @@ window.addEventListener('load', function(){
     canvas.height = 500;
 
     class InputHandler {
-
+        constructor(game){
+            this.game = game;
+            window.addEventListener('keydown', e => {
+                if (((e.key === 'ArrowUp') || (e.key === 'ArrowDown'))
+                 && this.game.keys.indexOf(e.key) === -1){
+                    this.game.keys.push(e.key);
+                }
+                console.log(this.game.keys);
+            });
+            window.addEventListener('keyup', e =>{
+                if (this.game.keys.indexOf(e.key) > -1){
+                    this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
+                }
+                console.log(this.game.keys);
+            })
+        }
     }
     class Projectime {
 
@@ -24,6 +39,9 @@ window.addEventListener('load', function(){
             this.speedY = 0
         }
         update(){
+            if (this.game.keys.includes('ArrowUp')) this.speedY = -10;
+            else if (this.game.keys.includes('ArrowDown')) this.speedY = 10;
+            else this.speedY = 0;
             this.y += this.speedY;
         }
         draw(context){
@@ -47,6 +65,8 @@ window.addEventListener('load', function(){
             this.width = width;
             this.height = height;
             this.player = new Player(this);
+            this.input = new InputHandler(this);
+            this.keys = [];
         }
         update(){
             this.player.update();
